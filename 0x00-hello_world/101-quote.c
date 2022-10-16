097f2eda797f0787590d5defd70aee5cf134321c
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2022-10-16 15:32:59 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(int argc, char** argv)
 {
-	write(STDOUT_FILENO, "and that piece or art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece or art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
