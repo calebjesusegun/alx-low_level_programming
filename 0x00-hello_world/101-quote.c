0d2fb2d86b1f71fee7fae6220811c25550f851d9
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2022-10-16 15:37:00 -0700

    100-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,7 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main(int argc, char** argv)
+int main(void)
 {
 	write(1, "and that piece or art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
