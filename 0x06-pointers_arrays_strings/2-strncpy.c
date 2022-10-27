1694331ed4b8bdf279ec9f4ab6e835df718685be
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2022-10-27 15:18:10 -0700

    "Third Task"

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,19 @@
 #include "main.h"
 /**
- * * _strncat - concatenates two strings,
+ * * _strncpy - copies a string
  * * @dest: destination.
  * * @src: source.
- * * @n: amount of bytes used from src.
+ * * @n: amount of bytes from src.
  * * Return: the pointer to dest.
  **/
-char *_strncat(char *dest, char *src, int n)
+
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+		for ( ; i < n; i++)
+			dest[i] = '\0';
+		return (dest);
 }
