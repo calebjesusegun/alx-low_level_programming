3bc1cb4c5bea05be00ba1359122c68fbc6a3c23a
calebjesusegun <jesuseguncaleb@gmail.com>
2022-10-28 03:03:56 -0700

    "Sixth Task"

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * * string_toupper - changes all lowercase letters of a string
  * * to uppercase
@@ -6,6 +7,7 @@
  * *
  * * Return: the pointer to dest.
  **/
+
 char *string_toupper(char *s)
 {
 	int count = 0;
