182124f039fc592a52e619ddd67aad81030c778e
calebjesusegun <jesuseguncaleb@gmail.com>
2022-10-28 03:27:14 -0700

    First Task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * * _strcat - concatenates two strings,
- * * @dest: destination.
- * * @src: source.
- * * Return: the pointer to dest.
- * *
+ ** _strcat - concatenates two strings,
+ ** @dest: destination.
+ ** @src: source.
+ ** Return: the pointer to dest.
+ **
  **/
 char *_strcat(char *dest, char *src)
 {
@@ -21,8 +20,8 @@ char *_strcat(char *dest, char *src)
 		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
-			count++;
-			count2++;
+		count++;
+		count2++;
 	}
 	return (dest);
 }
